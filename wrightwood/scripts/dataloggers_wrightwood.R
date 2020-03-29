5e11af48ae32c946fab3fe8643a8a4ede4fc74ee
Jenna Ekwealor <jtbe@berkeley.edu>
2020-03-28 18:16:11 -0700

    add scripts directory ;

---
+++ wrightwood/scripts/dataloggers_wrightwood.R
@@ -0,0 +1,459 @@
+library(reshape)
+library(lattice)
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+library(rstatix)
+library(ggpubr)
+library(patchwork)
+setwd("/Users/jennaekwealor/Documents/3dmoss/hypoliths/wrightwood/")
+
+source('../ibutton.functions.R')
+
+# load ibutton data
+ibutton.data.temp <- read.ibutton.folder("temp")
+ibutton.data.RH <- read.ibutton.folder("RH")
+
+## which ones failed? you could just check the number of rows in each, and pick 
+## the ones which are suspiciously short, indicating that the ibutton stopped 
+## recording temperature.  This function automates this process to make it a bit
+## more objective: it points out ones which recorded less than the median number
+## of datapoints for the experiment.  It assumes that all the ibuttons were
+## supposed to run for equal amounts of time
+# id.broken(ibutton.data.temp)
+# id.broken(ibutton.data.RH)
+
+## get the lengths for your own use:
+# sapply(ibutton.data.temp,nrow)
+# sapply(ibutton.data.RH,nrow)
+
+## sometimes, fieldworkers record the data incorrectly -- for example, one 
+## common mistake is to save data from the same ibutton twice with different 
+## filenames.  However, each ibutton has a unique registration number.  check 
+## for any number >1 in this table to identify this error. Additionally, if you
+## recorded the registration numbers (written on the back of the ibuttons) you
+## could use this to get them from the datafiles themselves
+
+# table(get.registration.numbers("temp"))
+# table(get.registration.numbers("RH"))
+
+## correct the dates and make df 
+## Now that the data is checked, this function takes the list of ibuttons and
+## combines them together.  It also reformats the "Date.Time" variable, into a
+## format that R recognizes as a date and time.
+#ibutton.df <- as.data.frame(ibutton.data)
+ibutton.df.temp <- na.omit(ibuttons.to.data.frame(ibutton.data.temp))
+head(ibutton.df.temp)
+summary(ibutton.df.temp)
+# remove row names
+rownames(ibutton.df.temp) <- c()
+# make new 'habitat' column
+ibutton.df.temp$habitat <- ifelse(grepl("surface", ibutton.df.temp$ibutton), "Surface", ifelse(grepl("hypo", ibutton.df.temp$ibutton), "Hypolithic", ""))
+
+ibutton.df.RH <- na.omit(ibuttons.to.data.frame(ibutton.data.RH))
+head(ibutton.df.RH)
+summary(ibutton.df.RH)
+# remove row names
+rownames(ibutton.df.RH) <- c()
+# make new 'treatment' column
+ibutton.df.RH$habitat <- ifelse(grepl("surface", ibutton.df.RH$ibutton), "Surface", ifelse(grepl("hypo", ibutton.df.RH$ibutton), "Hypolithic", ""))
+
+
+
+
+
+# fix format
+ibutton.df.temp$Date.Time <- as.POSIXct(ibutton.df.temp$Date.Time)
+ibutton.df.RH$Date.Time <- as.POSIXct(ibutton.df.RH$Date.Time)
+
+
+# temp
+
+############ pull out the high TEMP from each day, for each habitat ############
+ibutton.df.temp %>% 
+  mutate(date = date(Date.Time)) %>% 
+  group_by(date, habitat) %>% 
+  summarize(high = max(Value)) -> ibutton.df.temp.dailyhigh
+
+head(ibutton.df.temp.dailyhigh)
+
+ibutton.df.temp.dailyhigh %>% 
+  group_by(habitat) %>% 
+  summarize(meandailyhigh = mean(high), n = n(), sd.dailyhigh = sd(high)) -> ibutton.df.temp.dailyhigh.mean
+head(ibutton.df.temp.dailyhigh.mean)
+
+# # A tibble: 2 x 4
+# habitat  meandailyhigh     n sd.dailyhigh
+# <chr>            <dbl> <int>        <dbl>
+#   1 Hypolith          19.1   164         11.4
+# 2 Surface           21.7   164         12.5
+
+ggplot(ibutton.df.temp.dailyhigh, aes(habitat, high)) +
+  geom_boxplot()
+
+# high temp stats 
+pwc.ibutton.df.temp.dailyhigh <- ibutton.df.temp.dailyhigh %>% ungroup() %>% rstatix::t_test(high ~ habitat, paired = TRUE)
+
+pwc.ibutton.df.temp.dailyhigh
+
+# # A tibble: 1 x 8
+# .y.   group1   group2     n1    n2 statistic    df        p
+# * <chr> <chr>    <chr>   <int> <int>     <dbl> <dbl>    <dbl>
+#   1 high  Hypolith Surface   164   164     -16.6   163 5.69e-37
+
+anova.ibutton.df.temp.dailyhigh <-  ibutton.df.temp.dailyhigh %>% ungroup() %>% 
+  anova_test(dv=high, wid=date, within=habitat)
+
+get_anova_table(anova.ibutton.df.temp.dailyhigh)
+# ANOVA Table (type III tests)
+# 
+# Effect DFn DFd       F        p p<.05   ges
+# 1 habitat   1 163 276.885 5.69e-37     * 0.012
+
+# Visualization: box plots with p-values
+pwc.ibutton.df.temp.dailyhigh <- pwc.ibutton.df.temp.dailyhigh %>% add_xy_position(x = "habitat")
+
+
+
+
+# bxp.ibutton.df.temp.dailyhigh <- ggboxplot(
+#   ibutton.df.temp.dailyhigh, x = "habitat", y = "high",
+#   color = "habitat", palette = "Accent",
+#   add = "point"
+# )
+# bxp.ibutton.df.temp.dailyhigh
+# 
+# bxp.ibutton.df.temp.dailyhigh + 
+#   stat_pvalue_manual(pwc.ibutton.df.temp.dailyhigh, tip.length = 0, hide.ns = TRUE) +
+#   labs(
+#     subtitle = get_test_label(anova.ibutton.df.temp.dailyhigh, detailed = FALSE),
+#     caption = get_pwc_label(pwc.ibutton.df.temp.dailyhigh)
+#   )
+
+
+
+
+########### pull out the low TEMP from each day, for each habitat #############
+ibutton.df.temp %>% 
+  mutate(date = date(Date.Time)) %>% 
+  group_by(date, habitat) %>% 
+  summarize(low = min(Value), n = n()) -> ibutton.df.temp.dailylow
+head(ibutton.df.temp.dailylow)
+
+ibutton.df.temp.dailylow %>% 
+  group_by(habitat) %>% 
+  summarize(meandailylow = mean(low), n = n(), sd.dailylow = sd(low)) -> ibutton.df.temp.dailylow.mean
+head(ibutton.df.temp.dailylow.mean)
+
+# # A tibble: 2 x 4
+# habitat  meandailylow     n sd.dailylow
+# <chr>           <dbl> <int>       <dbl>
+#   1 Hypolith         3.79   164        5.28
+# 2 Surface          1.90   164        5.17
+
+ggplot(ibutton.df.temp.dailylow, aes(habitat, low)) +
+  geom_boxplot()
+
+# # A tibble: 2 x 4
+# habitat  meandailylow     n sd.dailylow
+# <chr>           <dbl> <int>       <dbl>
+#   1 Hypolith         7.98   154        5.35
+# 2 Surface          5.65   154        5.03
+
+####### low temp stats #######
+pwc.ibutton.df.temp.dailylow <- ibutton.df.temp.dailylow %>% ungroup() %>% rstatix::t_test(low ~ habitat, paired = TRUE)
+
+pwc.ibutton.df.temp.dailylow
+
+# # A tibble: 1 x 8
+# .y.   group1   group2     n1    n2 statistic    df        p
+# * <chr> <chr>    <chr>   <int> <int>     <dbl> <dbl>    <dbl>
+#   1 low   Hypolith Surface   164   164      23.6   163 1.69e-54
+
+anova.ibutton.df.temp.dailylow <-  ibutton.df.temp.dailylow %>% ungroup() %>% 
+  anova_test(dv=low, wid=date, within=habitat)
+
+get_anova_table(anova.ibutton.df.temp.dailylow)
+
+# Visualization: box plots with p-values
+pwc.ibutton.df.temp.dailylow <- pwc.ibutton.df.temp.dailylow %>% add_xy_position(x = "habitat")
+
+
+
+
+# bxp.ibutton.df.temp.dailylow + 
+#   stat_pvalue_manual(pwc.ibutton.df.temp.dailylow, tip.length = 0, hide.ns = TRUE) +
+#   labs(
+#     subtitle = get_test_label(anova.ibutton.df.temp.dailylow, detailed = FALSE),
+#     caption = get_pwc_label(pwc.ibutton.df.temp.dailylow)
+#   ) 
+
+
+
+
+####### RH ########
+
+# pull out the high RH from each day, for each habitat 
+ibutton.df.RH %>% 
+  mutate(date = date(Date.Time)) %>% 
+  group_by(date, habitat) %>% 
+  summarize(high = max(Value)) -> ibutton.df.RH.dailyhigh
+
+head(ibutton.df.RH.dailyhigh)
+
+ibutton.df.RH.dailyhigh %>% 
+  group_by(habitat) %>% 
+  summarize(meandailyhigh = mean(high), n = n(), sd.dailyhigh = sd(high)) -> ibutton.df.RH.dailyhigh.mean
+head(ibutton.df.RH.dailyhigh.mean)
+
+# # A tibble: 2 x 4
+# habitat  meandailyhigh     n sd.dailyhigh
+# <chr>            <dbl> <int>        <dbl>
+#   1 Hypolith          70.4   164         34.6
+# 2 Surface           70.8   164         27.4
+
+ggplot(ibutton.df.RH.dailyhigh, aes(habitat, high)) +
+  geom_boxplot()
+
+# high RH stats 
+pwc.ibutton.df.RH.dailyhigh <- ibutton.df.RH.dailyhigh %>% ungroup() %>% rstatix::t_test(high ~ habitat, paired = TRUE)
+
+pwc.ibutton.df.RH.dailyhigh
+# # A tibble: 1 x 12
+# .y.   group1   group2     n1    n2 statistic    df     p y.position groups     xmin  xmax
+# <chr> <chr>    <chr>   <int> <int>     <dbl> <dbl> <dbl>      <dbl> <list>    <int> <int>
+#   1 high  Hypolith Surface   164   164    -0.337   163 0.736        100 <chr [2]>     1     2
+
+anova.ibutton.df.RH.dailyhigh <-  ibutton.df.RH.dailyhigh %>% ungroup() %>% 
+  anova_test(dv=high, wid=date, within=habitat)
+
+get_anova_table(anova.ibutton.df.RH.dailyhigh)
+
+# Visualization: box plots with p-values
+pwc.ibutton.df.RH.dailyhigh <- pwc.ibutton.df.RH.dailyhigh %>% add_xy_position(x = "habitat")
+
+
+
+
+
+
+
+
+# bxp.ibutton.df.RH.dailyhigh <- ggboxplot(
+#   ibutton.df.RH.dailyhigh, x = "habitat", y = "high",
+#   color = "habitat", palette = "Accent",
+#   add = "point"
+# )
+# bxp.ibutton.df.RH.dailyhigh
+# 
+# bxp.ibutton.df.RH.dailyhigh + 
+#   stat_pvalue_manual(pwc.ibutton.df.RH.dailyhigh, tip.length = 0, hide.ns = TRUE) +
+#   labs(
+#     subtitle = get_test_label(anova.ibutton.df.RH.dailyhigh, detailed = FALSE),
+#     caption = get_pwc_label(pwc.ibutton.df.RH.dailyhigh)
+#   )
+
+
+
+
+
+
+###### pull out the low RH from each day, for each habitat ######
+ibutton.df.RH %>% 
+  mutate(date = date(Date.Time)) %>% 
+  group_by(date, habitat) %>% 
+  summarize(low = min(Value)) -> ibutton.df.RH.dailylow
+
+head(ibutton.df.RH.dailylow)
+
+ibutton.df.RH.dailylow %>% 
+  group_by(habitat) %>% 
+  summarize(meandailylow = mean(low), n = n(), sd.dailylow = sd(low)) -> ibutton.df.RH.dailylow.mean
+head(ibutton.df.RH.dailylow.mean)
+
+# # A tibble: 2 x 4
+# habitat  meandailylow     n sd.dailylow
+# <chr>           <dbl> <int>       <dbl>
+#   1 Hypolith         62.5   164        39.2
+# 2 Surface          32.5   164        30.6
+
+ggplot(ibutton.df.RH.dailylow, aes(habitat, low)) +
+  geom_boxplot()
+
+# low RH stats 
+pwc.ibutton.df.RH.dailylow <- ibutton.df.RH.dailylow %>% ungroup() %>% rstatix::t_test(low ~ habitat, paired = TRUE)
+
+pwc.ibutton.df.RH.dailylow
+
+# # A tibble: 1 x 12
+# .y.   group1   group2     n1    n2 statistic    df        p y.position groups     xmin  xmax
+# <chr> <chr>    <chr>   <int> <int>     <dbl> <dbl>    <dbl>      <dbl> <list>    <int> <int>
+#   1 low   Hypolith Surface   164   164      13.1   163 2.84e-27        100 <chr [2]>     1     2
+
+anova.ibutton.df.RH.dailylow <-  ibutton.df.RH.dailylow %>% ungroup() %>% 
+  anova_test(dv=low, wid=date, within=habitat)
+
+get_anova_table(anova.ibutton.df.RH.dailylow)
+
+# Visualization: box plots with p-values
+pwc.ibutton.df.RH.dailylow <- pwc.ibutton.df.RH.dailylow %>% add_xy_position(x = "habitat")
+
+
+
+
+
+# bxp.ibutton.df.RH.dailylow <- ggboxplot(
+#   ibutton.df.RH.dailylow, x = "habitat", y = "low",
+#   color = "habitat", palette = "Accent",
+#   add = "point"
+# )
+# bxp.ibutton.df.RH.dailylow
+# 
+# bxp.ibutton.df.RH.dailylow + 
+#   stat_pvalue_manual(pwc.ibutton.df.RH.dailylow, tip.length = 0, hide.ns = TRUE) +
+#   labs(
+#     subtitle = get_test_label(anova.ibutton.df.RH.dailylow, detailed = FALSE),
+#     caption = get_pwc_label(pwc.ibutton.df.RH.dailylow)
+#   )
+
+###### FIGURES ######
+
+
+sigfontsize <- 6
+meanfontsize <- 4.25
+xtitlefontsize <- 18
+ytitlefontsize <- 18
+xaxisfontsize <- 14
+yaxisfontsize <- 14
+
+
+
+bxp.ibutton.df.temp.dailyhigh <- ggboxplot(
+  ibutton.df.temp.dailyhigh, x = "habitat", y = "high",
+  fill = "habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = expression("Mean daily high temperature " ( degree~C)),  
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = TRUE, method = "t.test", 
+                     label.y = 45.5, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=50, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize) +
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2)
+
+
+#bxp.ibutton.df.temp.dailyhigh
+
+
+
+bxp.ibutton.df.temp.dailylow <- ggboxplot(
+  ibutton.df.temp.dailylow, x = "habitat", y = "low",
+  fill = "habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = expression("Mean daily low temperature " ( degree~C)),  
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = TRUE, method = "t.test", 
+                     label.y = 45, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=50, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize) +
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2)
+
+
+#bxp.ibutton.df.temp.dailylow
+
+
+# figure 2
+figure2 <- (bxp.ibutton.df.temp.dailylow + bxp.ibutton.df.temp.dailyhigh)
+
+
+pdf("paper/Figure 2.pdf") 
+
+figure2
+
+dev.off()
+
+
+
+bxp.ibutton.df.RH.dailyhigh <- ggboxplot(
+  ibutton.df.RH.dailyhigh, x = "habitat", y = "high",
+  fill = "habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = expression("Mean daily high relative humidity (%)"),
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = TRUE, method = "t.test", 
+                     label.y = 102.5, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=107, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize)+
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2) + 
+  scale_y_continuous(breaks=seq(0,100,20))
+                                                                                            
+
+
+#bxp.ibutton.df.RH.dailyhigh
+
+
+
+
+
+bxp.ibutton.df.RH.dailylow <- ggboxplot(
+  ibutton.df.RH.dailylow, x = "habitat", y = "low",
+  fill = "habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = expression("Mean daily low relative humidity (%)"),
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = TRUE, method = "t.test", 
+                     label.y = 101, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=107, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize)+
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2)  + 
+  scale_y_continuous(breaks=seq(0,100,20))
+
+
+#bxp.ibutton.df.RH.dailylow
+
+# figure 3
+figure3 <- (bxp.ibutton.df.RH.dailylow + bxp.ibutton.df.RH.dailyhigh)
+
+
+
+pdf("paper/Figure 3.pdf") 
+
+figure3
+
+dev.off()
+
---
+++ wrightwood/scripts/stems.R
@@ -0,0 +1,175 @@
+
+library(ggplot2)
+library(tidyverse)
+library(rstatix)
+library(ggpubr)
+library(infer)
+setwd("/Users/jennaekwealor/Documents/3dmoss/hypoliths/wrightwood/")
+
+
+sigfontsize <- 6
+meanfontsize <- 4.25
+xtitlefontsize <- 18
+ytitlefontsize <- 18
+xaxisfontsize <- 14
+yaxisfontsize <- 14
+
+
+####### QUARTZ THICKNESS #######
+
+quartz.data <- as.data.frame(read.csv("quartz-thickness.csv"))
+mean(quartz.data$thickness)
+# 25.63889
+sd(quartz.data$thickness)
+# 14.88762
+
+####### COMMUNITY COMPOSITION  #######
+
+community.data <- as.data.frame(read.csv("community.csv"))  %>% 
+  select (Habitat, Species) %>% na.omit()
+community.data$Habitat <- factor(community.data$Habitat, levels = c("Hypolithic", "Surface"))
+
+
+community.data.table <- table(community.data$Habitat, community.data$Species)
+community.data.table %>% rstatix::fisher_test()
+
+# # A tibble: 1 x 3
+# n       p p.signif
+# * <int>   <dbl> <chr>   
+#   1    53 0.00257 **   
+
+community.data %>% count(Species, Habitat) 
+
+
+####### SHOOT LENGTH #######
+length.data <- as.data.frame(read.csv("stem-length.csv"))
+length.data <- length.data %>% tidyr::gather(Habitat, Length, Surface:Hypolithic) %>% select (Habitat, Length) %>% na.omit()
+length.data$Habitat <- factor(length.data$Habitat, levels = c("Hypolithic", "Surface"))
+
+
+# shapiro_test(length.data$length) # not normal
+# hist(length.data$length)
+
+pwc.length <- length.data %>% 
+  wilcox_test(Length ~ Habitat)
+
+pwc.length
+# # A tibble: 1 x 7
+# .y.    group1     group2     n1    n2 statistic             p
+# * <chr>  <chr>      <chr>   <int> <int>     <dbl>         <dbl>
+#   1 length hypolithic surface    50   299     11390 0.00000000306
+
+
+# ggboxplot(length.data, x = "habitat", y = "length", add = "point")
+
+#pwc.length <- pwc.length %>% add_xy_position(x = "habitat")
+
+# bxp.length.data <- ggboxplot(
+#   length.data, x = "habitat", y = "length",
+#   color = "habitat", palette = "Accent",
+#   add = "point",
+# )
+# bxp.length.data + 
+#   stat_pvalue_manual(pwc.length, tip.length = 0, hide.ns = TRUE) +
+#   labs(caption = get_pwc_label(pwc.length)) +
+#   theme(legend.position = "none") 
+
+
+bxp.length.data <- ggboxplot(
+  length.data, x = "Habitat", y = "Length",
+  fill = "Habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = "Shoot length (mm)",
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = FALSE, method = "wilcox.test", 
+                     label.y = 6, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=6.5, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize)+
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2) 
+
+
+pdf("paper/Figure 4.pdf") 
+
+figure4 <- bxp.length.data
+figure4
+
+dev.off()
+
+
+
+
+
+####### LEAF DENSITY  #######
+density.data <- as.data.frame(read.csv("leaf-density.csv"))
+density.data <- na.omit(density.data)
+
+#shapiro_test(density.data$leaf.density) # not normal
+#hist(density.data$leaf.density)
+
+pwc.density <- density.data %>% 
+  wilcox_test(leaf.density ~ habitat)
+
+pwc.density
+# 
+# # A tibble: 1 x 7
+# .y.          group1     group2     n1    n2 statistic      p
+# * <chr>        <chr>      <chr>   <int> <int>     <dbl>  <dbl>
+#   1 leaf.density hypolithic surface    10    23        52 0.0125
+
+
+# ggboxplot(density.data, x = "habitat", y = "leaf.density", add = "point")
+
+#pwc.density <- pwc.density %>% add_xy_position(x = "habitat")
+
+# bxp.density.data <- ggboxplot(
+#   density.data, x = "habitat", y = "leaf.density",
+#   color = "habitat", palette = "Accent",
+#   add = "point",
+# )
+# bxp.density.data + 
+#   stat_pvalue_manual(pwc.density, tip.length = 0, hide.ns = TRUE) +
+#   labs(caption = get_pwc_label(pwc.density)) +
+#   theme(legend.position = "none") 
+
+
+bxp.density.data <- ggboxplot(
+  density.data, x = "habitat", y = "leaf.density",
+  fill = "habitat", palette = "Accent",
+  #add = "boxplot",add.params = list(fill = "white"),
+  ylab = "Leaf density (leaves/mm)",
+  xlab = "Microhabitat", 
+  font.x = xtitlefontsize,
+  font.y = ytitlefontsize
+)  +
+  font("x.text", size = xaxisfontsize) +
+  font("y.text", size = yaxisfontsize) +
+  stat_compare_means(aes(label = ..p.signif..), 
+                     paired = FALSE, method = "wilcox.test", 
+                     label.y = 60, 
+                     label.x.npc = 0.45, 
+                     size = sigfontsize
+  ) +
+  rremove("legend") +
+  stat_summary(fun.data = function(x) data.frame(y=65, label = paste("Mean =", format(round(mean(x), digits = 1), nsmall = 1))), geom="text", size = meanfontsize)+
+  geom_jitter(position=position_jitter(width=.07, height=0), alpha = 0.3, size = 2) 
+
+
+
+
+pdf("paper/Figure 5.pdf") 
+
+figure5 <- bxp.density.data
+figure5
+
+dev.off()
+
+
